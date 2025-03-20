938a2fc764c6ee4b40a58da6f074cde2d42da1dc
mohammad javad <chenarbeny2005@gmail.com>
2025-03-20 23:43:26 +0330

    compelete

--- Answers/Main.java
+++ Answers/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean replay;
+
         do {
             System.out.println("Enter the grid size: ");
             int gridSize = scanner.nextInt();
